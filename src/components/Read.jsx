bf720cc4b9afd0673a06adfc0862f7a7c2f22b88
Renuka-M-Anjutagi <162134316+Renuka-M-Anjutagi@users.noreply.github.com>
2024-05-15 01:12:44 +0530

    Update Read.jsx

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -9,7 +9,7 @@ function Read() {
     const {id} = useParams();
      useEffect (() => {
         axios.get('http://localhost:3000/users/' + id)
-        .then(res => setData(res.data))
+        .then(res => setData(res.data.users))
         .catch(error => console.error(error));
      },[])
   return (
