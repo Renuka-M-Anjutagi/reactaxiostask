eb9f9a0ed1b8a4a46f3f902e0f9ce134f8b13053
Renuka-M-Anjutagi <162134316+Renuka-M-Anjutagi@users.noreply.github.com>
2024-05-20 12:27:42 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 const [data,setData] = useState([])
      useEffect (() => {
         axios.get('https://jsonplaceholder.typicode.com/users/')
-        .then(res => setData(res.data.users))
+        .then(res => setData(res.data))
         .catch(error => console.error(error));
      },[])
 
