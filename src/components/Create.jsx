282149a3e9ee495b28c86feb6e4ef4faad1c192d
Renuka-M-Anjutagi <162134316+Renuka-M-Anjutagi@users.noreply.github.com>
2024-05-20 12:31:09 +0530

    Update Create.jsx

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -14,7 +14,7 @@ function Create() {
     const navigate = useNavigate();
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios.post('http://localhost:3000/users' , values)
+        axios.post('https://jsonplaceholder.typicode.com/users' , values)
         .then(res => {
             console.log(res);
             navigate('/')
